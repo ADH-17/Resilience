6d618777f076e898ba362b94628627b0c24b90b7
ADH-17 <ayden.humphries17@gmail.com>
2025-04-25 09:57:08 -0700

    changed the popup blocker and used a title_top_center class

--- content-filter-extension/content.js
+++ content-filter-extension/content.js
@@ -13,11 +13,12 @@ const blockedSites = [
     document.documentElement.innerHTML = ` 
     <div style="display: flex; height: 100vh; align-items: center; justify-content: center; text-align: center; background-color: #111; color: white; font-family: sans-serif;">
       <div>
-        <h1>Stay Strong 💪</h1>
+        <h1> class="title_top_center">STOP! DANGER AHEAD!</h1>
+        <h2>Stay Strong 💪</h1>
         <p>This site is blocked by <strong>Resilience</strong>.</p>
         <p>You're in control. Remember why you're fighting.</p>
 
-        <h2>Resources to keep you on track:</h2>
+        <h3>Resources to keep you on track:</h2>
         <a href= "https://nofap.com/">Visit nofap.com</a>
       </div>
     </div>
